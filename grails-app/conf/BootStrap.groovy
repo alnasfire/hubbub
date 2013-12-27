bf0296ae1486b2e5a558162c724e218878c9afce
alnasfire <alnasfire@gmail.com>
2013-12-27 18:15:29 +0300

    sdf

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,31 @@
+import grails.util.Environment
+import com.grailsinaction.Profile
+import com.grailsinaction.User
+
 class BootStrap {
 
     def init = { servletContext ->
+        switch(Environment.current){
+            case Environment.DEVELOPMENT:
+                createAdminUserIfRequired()
+                break;
+
+            case Environment.PRODUCTION:
+                println "No spesial configuration required"
+                break;
     }
+    }
+
     def destroy = {
+    }
+
+    void createAdminUserIfRequired(){
+        if(!User.findByUserId("admin")){
+            println "Fresh Database. Creating ADMIN user."
+            def profile = new Profile(email: "admin@yourhost.com")
+            def user = new User(userId: "admin", password: "secret", profile: profile).save()
+        } else {
+            println "Existing admin user, skipping creation."
+        }
     }
 }
